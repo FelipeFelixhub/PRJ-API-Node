c1c58d77a5ec7ca520a46ba836d33cb194f6fcf1
Felipe Felix <115411900+FelipeFelixhub@users.noreply.github.com>
2024-03-11 23:18:38 +0000

    Add env

--- index.js
+++ index.js
@@ -8,10 +8,11 @@ const port = 3000;
 // Configuração do MySQL
 const connection = mysql.createConnection({
   host: 'localhost',
-  user: 'root', // Seu nome de usuário do MySQL
-  password: 'mysql@123', // Sua senha do MySQL
-  database: 'usersdb'
+  user: "root",//process.env.MYSQL_USER, // Usa variáveis de ambiente
+  password: "mysql@123",//process.env.MYSQL_PASSWORD,
+  database: "usersdb"//process.env.MYSQL_DATABASE
 });
+
 
 // Conectar ao banco de dados
 connection.connect((err) => {
@@ -30,6 +31,7 @@ app.post('/users', (req, res) => {
   const INSERT_USER_QUERY = `INSERT INTO users (name, email) VALUES (?, ?)`;
   connection.query(INSERT_USER_QUERY, [name, email], (err, results) => {
     if (err) throw err;
+    res.statusCode=201;
     res.send('Usuário criado com sucesso');
   });
 });
@@ -58,3 +60,5 @@ app.get('/users/:id', (req, res) => {
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
 });
+
+module.exports = app;
